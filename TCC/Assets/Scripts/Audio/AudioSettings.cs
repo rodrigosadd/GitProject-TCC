9b44a14db6bf924a99ff23d06dcc9c6dfafbfd1b
Iago Martins <53104712+iagomartins@users.noreply.github.com>
2020-10-14 16:15:58 -0300

    Menu Audio Controller

--- TCC/Assets/Scripts/Audio/AudioSettings.cs
+++ TCC/Assets/Scripts/Audio/AudioSettings.cs
@@ -1,52 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using UnityEngine;
 
-public class AudioSettings : MonoBehaviour
-{
+public class AudioSettings : MonoBehaviour {
     EventInstance SFXVolumeTestEvent;
 
     Bus Music;
     Bus SFX;
     Bus Master;
-    float musicVolume;
-    float SFXVolume;
-    float masterVolume;
+    float musicVolume = 1.0f;
+    float SFXVolume = 1.0f;
+    float masterVolume = 1.0f;
     // Start is called before the first frame update
-    void Awake()
-    {
-        Music = RuntimeManager.GetBus("bus:Master/Music");
-        SFX = RuntimeManager.GetBus("bus:Master/SFX");
-        Master = RuntimeManager.GetBus("bus:Master");
-        SFXVolumeTestEvent = RuntimeManager.CreateInstance("event:Pickaxe");
+    void Awake () {
+        Music = RuntimeManager.GetBus ("bus:/Master/Music");
+        SFX = RuntimeManager.GetBus ("bus:/Master/SFX");
+        Master = RuntimeManager.GetBus ("bus:/Master");
+        SFXVolumeTestEvent = RuntimeManager.CreateInstance ("event:/Pickaxe");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Music.setVolume(musicVolume);
-        SFX.setVolume(SFXVolume);
-        Master.setVolume(masterVolume);
+    void Update () {
+        Music.setVolume (musicVolume);
+        SFX.setVolume (SFXVolume);
+        Master.setVolume (masterVolume);
     }
 
-    public void MusicVolumeLevel(float newVolume)
-    {
+    public void MusicVolumeLevel (float newVolume) {
         musicVolume = newVolume;
     }
-    public void SFXVolumeLevel(float newVolume)
-    {
+    public void SFXVolumeLevel (float newVolume) {
         SFXVolume = newVolume;
 
         //Toca o som durante o ajuste de volume.
         PLAYBACK_STATE pbState;
-        SFXVolumeTestEvent.getPlaybackState(out pbState);
+        SFXVolumeTestEvent.getPlaybackState (out pbState);
         if (pbState != PLAYBACK_STATE.PLAYING)
-            SFXVolumeTestEvent.start();
+            SFXVolumeTestEvent.start ();
     }
-    public void MasterVolumeLevel(float newVolume)
-    {
+    public void MasterVolumeLevel (float newVolume) {
         masterVolume = newVolume;
     }
 }
