61eb2786f2729f702726bf3b131c0d7f6120a587
Rodrigo Souza <rodrigo_souza174@hotmail.com>
2020-11-19 18:08:33 -0300

    Checkpoint and menu
    Checkpoint and more menu settings implemented (Graphics, fullscreen and quality).

---
+++ TCC/Assets/Scripts/Level/Checkpoint/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+     public Transform spot;
+
+     void OnTriggerEnter(Collider collider)
+     {
+          if (PlayerController.instance.death.currentPoint != spot)
+          {
+               if (collider.tag == "Player")
+               {
+                    PlayerController.instance.death.currentPoint = spot;
+                    Debug.Log("entrou");
+               }
+          }
+     }
+}
--- TCC/Assets/Scripts/Menu/MenuController.cs
+++ TCC/Assets/Scripts/Menu/MenuController.cs
@@ -3,16 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-     public void LoadScene(int Num_Scene)
+     public Dropdown resolutionDropdown;
+
+     private Resolution[] _resolutions;
+
+     void Start()
      {
-          SceneManager.LoadScene(Num_Scene);
+          GetResolutions();
+     }
+
+     public void LoadScene(int IndexScene)
+     {
+          SceneManager.LoadScene(IndexScene);
      }
 
      public void QuitGame()
      {
           Application.Quit();
+     }
+
+     public void SetFullscrenn(bool isFullscreen)
+     {
+          Screen.fullScreen = isFullscreen;
+     }
+
+     void GetResolutions()
+     {
+          _resolutions = Screen.resolutions;
+
+          resolutionDropdown.ClearOptions();
+
+          List<string> options = new List<string>();
+
+          int currentResolutionIndex = 0;
+          for (int i = 0; i < _resolutions.Length; i++)
+          {
+               string option = _resolutions[i].width + "x" + _resolutions[i].height;
+               options.Add(option);
+
+               if (_resolutions[i].width == Screen.currentResolution.width &&
+                  _resolutions[i].height == Screen.currentResolution.height)
+               {
+                    currentResolutionIndex = i;
+               }
+          }
+
+          resolutionDropdown.AddOptions(options);
+          resolutionDropdown.value = currentResolutionIndex;
+          resolutionDropdown.RefreshShownValue();
+     }
+
+     public void SetResolutions(int resolutionIndex)
+     {
+          Resolution resolution = _resolutions[resolutionIndex];
+          Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+     }
+
+     public void SetQuality(int qualityIndex)
+     {
+          QualitySettings.SetQualityLevel(qualityIndex);
      }
 }
